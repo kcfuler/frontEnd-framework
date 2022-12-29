88cc44c1c6a2650e6c53684665c63a1d7aa7ea8b
kcp <2842961263@qq.com>
2022-12-29 11:47:56 +0800

    feat: add the router & query

--- React/self-practice/setup/a-react-app/src/App.jsx
+++ React/self-practice/setup/a-react-app/src/App.jsx
@@ -1,11 +1,28 @@
-import SearchParams from './SearchParams'
+import SearchParams from "./SearchParams";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Details from "./Details";
+
+const queryClient = QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
-      <SearchParams></SearchParams>
-    </>
-  )
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ React/self-practice/setup/a-react-app/src/Details.jsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom";
+
+export default function Details() {
+  const { id } = useParams();
+
+  return (
+    <>
+      <h1> Details </h1>
+      <h2> {id}</h2>
+    </>
+  );
+}
