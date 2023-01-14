7498f27132fa2a4cb99b90180102584714d57586
kcp <2842961263@qq.com>
2023-01-14 14:20:55 +0800

    feat: use loading

--- React/Next/next-app-1/src/app/blog/[...slug]/page.tsx
+++ React/Next/next-app-1/src/app/blog/[...slug]/page.tsx
@@ -1,9 +1,15 @@
-const getData = async (slug) => {
-  const data = await getDataFromCMS(slug);
-  return data;
-};
+// const getData = async (slug) => {
+//   const data = await getDataFromCMS(slug);
+//   return data;
+// };
+
+const delay = (time: number) =>
+  new Promise((resolve) => {
+    setTimeout(() => resolve(1), time);
+  });
 
 export default async function BlogPost({ params }) {
+  const temp = await delay(5000);
   console.log(params);
   // const post = await getData();
   return <div>blog post</div>;
