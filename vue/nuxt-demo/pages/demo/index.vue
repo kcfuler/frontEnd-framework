18a6426f6fd75f5e68ad3944983e119deb0de9fd
kcp <2842961263@qq.com>
2023-02-02 23:13:19 +0800

    feat: 使用动态参数和编程导航

---
+++ vue/nuxt-demo/pages/demo/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    this is the demo index file
+    <button @click="handleClick">点我获取数据并跳转</button>
+  </div>
+</template>
+
+<script setup lang='ts'>
+
+async function handleClick(){
+  const data = await $fetch('/api/getId')
+  navigateTo({
+    path: `/users-test/${data.id}`,
+  })
+  console.log(data.id);
+}
+
+</script>
+
+<style scoped lang='scss'>
+
+</style>
---
+++ vue/nuxt-demo/pages/users-[group]/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ testText }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+let testText = route.params.id;
+console.log(route.params);
+
+</script>
+
+<style scoped lang='scss'>
+
+</style>
