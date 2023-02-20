1dbbd238cb8b0aa3cbf095ca3073f5f67cbc16c0
kcp <2842961263@qq.com>
2023-02-21 00:52:23 +0800

    feat: update some thing

---
+++ React/react-test/src/component/TestComponent.tsx
@@ -0,0 +1,7 @@
+export default function TestComponent() {
+  return (
+    <div className="flex ">
+      test component
+    </div>
+  )
+}
---
+++ React/react-test/src/component/common/PathInput.tsx
@@ -0,0 +1,7 @@
+export default function PathInput() {
+  return (
+    <div>
+      this is pathInput
+    </div>
+  )
+}
