418cf90768687d83842864d4cf66509bb4a8e394
kcp <2842961263@qq.com>
2023-02-21 10:37:56 +0800

    fix: 修复tailwind

--- React/react-test/src/component/TestComponent.tsx
+++ React/react-test/src/component/TestComponent.tsx
@@ -1,7 +1,12 @@
+import MyMenu from './common/MyMenu'
+import PathInput from './common/PathInput'
+
 export default function TestComponent() {
   return (
-    <div className="flex ">
-      test component
-    </div>
+    <div className=" w-96 mx-auto font-bold">
+      test TestComponent
+      <PathInput />
+      <MyMenu></MyMenu>
+      </div>
   )
 }
--- React/react-test/src/main.tsx
+++ React/react-test/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import '@arco-design/web-react/dist/css/arco.css'
+import './index.css' // 导入taiwind
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
