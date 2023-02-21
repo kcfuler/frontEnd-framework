ff8924ca30da253ceddfe22a09ebf975555a30d5
kcp <2842961263@qq.com>
2023-02-21 23:16:59 +0800

    添加naive-ui

--- vue/vue-test/src/App.vue
+++ vue/vue-test/src/App.vue
@@ -2,13 +2,14 @@
 <!--  tailwind-->
 <!--  <Layout></Layout>-->
   <TestComponent></TestComponent>
-
+  <TestNaive></TestNaive>
 </template>
 
 <script setup lang="ts">
 import BoxDecoration from "./tailwind/BoxDecoration.vue";
 import Layout from "./tailwind/Layout.vue";
 import TestComponent from "./components/TestComponent.vue";
+import TestNaive from "./naive/TestNaive.vue";
 </script>
 
 <style scoped></style>
