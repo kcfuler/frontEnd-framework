7de726d156928e1378581b4855b052aba281808d
kcp <2842961263@qq.com>
2023-03-10 17:28:17 +0800

    feat: 模块、自定义模块

--- rest/start/demo/src/app.controller.ts
+++ rest/start/demo/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './user/user.service';
 
 @Controller('get')
 export class AppController {
-  constructor(@Inject('ABC') private readonly appService: AppService) {}
+  constructor(
+    @Inject('ABC') private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get('hello')
   getHello(): string {
-    return this.appService.getHello();
+    return this.userService.findAll(); // 通过公有模块的形式使用子模块中的方法
   }
 }
--- rest/start/demo/src/user/user.module.ts
+++ rest/start/demo/src/user/user.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
+@Global() // 将模块注册为全局模块, 这样就可以直接在别的控制器内使用模块功能，而不用在模块部分导入
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
+  exports: [UserService], // 当你有模块需要共享的时候，需要将它定义到共享模块中
 })
 export class UserModule {}
