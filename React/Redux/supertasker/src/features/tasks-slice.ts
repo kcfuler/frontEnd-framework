c7e62482cae93a8531d1ab608b774a78f6e0ebe7
kcp <2842961263@qq.com>
2023-04-15 23:50:14 +0800

    update: solid starter

--- React/Redux/supertasker/src/features/tasks-slice.ts
+++ React/Redux/supertasker/src/features/tasks-slice.ts
@@ -1,5 +1,21 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
 // const initialState = { enterties: []} // 这个形式并不是很好
-type TaskState = {
+export type TaskState = {
   entities: Task[];
-  
 }
+
+const initialState: TaskState = {
+  entities: []
+}
+
+const taskSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state, action: PayloadAction<Task>) =>{
+      state.entities.push(action.payload)
+    },
+    removeTask: (state) => state,
+  }
+})
